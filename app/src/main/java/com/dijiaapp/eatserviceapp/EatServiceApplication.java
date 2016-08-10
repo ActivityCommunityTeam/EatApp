d49899b957a6076c2aa6e869756a33039279dcf7
weijy bf Mac <weijy2012@gmail.com>
2016-08-10 09:39:52 +0800

    添加了登录界面

---
+++ app/src/main/java/com/dijiaapp/eatserviceapp/EatServiceApplication.java
@@ -0,0 +1,24 @@
+package com.dijiaapp.eatserviceapp;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by wjy on 16/8/9.
+ *
+ */
+public class EatServiceApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+
+
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(realmConfig);
+    }
+}
